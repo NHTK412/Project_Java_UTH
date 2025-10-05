b2e83bc54b64440899d49bb73d0caed4d12f88e5
Tuấn Khang <nguyenhuutuankhang412@gmail.com>
2025-10-05 23:26:55 +0700

    refactor(project): restructure folders for new Spring Boot setup

---
+++ src/main/java/com/example/evsalesmanagement/EvsalesmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.evsalesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvsalesmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvsalesmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evsalesmanagement/EvsalesmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evsalesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvsalesmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
